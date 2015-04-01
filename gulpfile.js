54219c658806adc8d49b3d0fa631182a2d335ab9
adamtuliper <adam.tuliper@gmail.com>
2015-04-01 01:24:36 -0700

    gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
+//initial source http://engineroom.teamwork.com/hassle-free-third-party-dependencies/
+
 var gulp = require('gulp');  
 var concat = require('gulp-concat');  
 var filter = require('gulp-filter'); 
 var uglify = require('gulp-uglify');
+var inject = require('gulp-inject');
 
 var mainBowerFiles = require('main-bower-files');
 
@@ -12,16 +15,15 @@ var filterByExtension = function(extension){
 };
 
 
-gulp.task('default', function(){  
+gulp.task('ConcatAndUglify', function () {
     var mainFiles = mainBowerFiles();
 
-    if(!mainFiles.length){
+    if (!mainFiles.length) {
         // No main files found. Skipping....
         return;
     }
 
-    mainFiles.forEach(function(entry)
-    {
+    mainFiles.forEach(function (entry) {
         console.log(entry);
     }
     )
@@ -29,11 +31,18 @@ gulp.task('default', function(){
 
     return gulp.src(mainFiles)
         .pipe(jsFilter)
-        .pipe(concat('concat-scripts.js'))
+        .pipe(concat('third-party-scripts.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./app'))
         .pipe(jsFilter.restore())
         .pipe(filterByExtension('css'))
-        .pipe(concat('concat-styles.css'))
+        .pipe(concat('third-party-styles.css'))
         .pipe(gulp.dest('./app'));
+});
+
+gulp.task('default',['ConcatAndUglify'], function(){  
+    //inject scripts AFTER concat step is done.
+    gulp.src('./*.html')
+    .pipe(inject(gulp.src('./app/**/*.js', { read: false }), { relative: true }))
+    .pipe(gulp.dest('./app'));
 });
